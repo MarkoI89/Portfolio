a857237a0461eb838242633823e4225d43ca2495
Marko Ivezic <markoivezic89@gmail.com>
2023-10-07 17:45:54 +0200

    Changed some text style for mobile version and contact form  functionality

--- frontend/src/pages/contactPage/Contact.jsx
+++ frontend/src/pages/contactPage/Contact.jsx
@@ -1,9 +1,8 @@
 import NavBar from "../../components/navBar/NavBar";
 import "./contact.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import { colors } from "@mui/material";
 
 function Contact() {
   const [name, setName] = useState("");
@@ -11,18 +10,47 @@ function Contact() {
   const [message, setMessage] = useState("");
   const [messageWarning, setMessageWarning] = useState(null);
   const [messageSent, setMessageSent] = useState(null);
+  const [nameError, setNameError] = useState(true);
+  const [emailError, setEmailError] = useState(true);
+  const [messageError, setMessageError] = useState(true);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
   const form = useRef();
+
+  const handleNameField = (e) => {
+    setName(e.target.value);
+    if (name !== "") {
+      setNameError(false);
+    } else {
+      setNameError(false);
+    }
+  };
+
+  const handleEmailField = (e) => {
+    setEmail(e.target.value);
+    if (email !== "") {
+      setEmailError(false);
+    } else {
+      setEmailError(false);
+    }
+  };
+  const handleMessageField = (e) => {
+    setMessage(e.target.value);
+    if (message !== "") {
+      setMessageError(false);
+    } else {
+      setMessageError(false);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (name === "" || email === "" || message === "") {
-      setMessageSent(null);
-      setMessageWarning(true);
+    if (nameError || emailError || messageError) {
+      setShowErrorMessage(true);
       return;
     } else {
-      setMessageWarning(null);
+      // setMessageWarning(null);
       emailjs
         .sendForm(
           import.meta.env.VITE_SERVICE_ID,
@@ -51,33 +79,45 @@ function Contact() {
           <hr />
           <h4>Fill the form below if you have any question or offer.</h4>
         </div>
-        {messageWarning && (
+        {/* {messageWarning && (
             <p style={{ color: "red" }}>All the fields are required</p>
-          )}
-          {messageSent && <p style={{color: "#42db55"}} >Message has been sent</p>}
+          )} */}
+        {messageSent && (
+          <p style={{ color: "#42db55" }}>Message has been sent</p>
+        )}
         <form className="form" ref={form} onSubmit={handleSubmit}>
+          {nameError && showErrorMessage && (
+            <p style={{ color: "red" }}>This field is required</p>
+          )}
           <input
             placeholder="Name"
             type="text"
-            onChange={(e) => setName(e.target.value)}
+            onChange={handleNameField}
             name="name"
+            className={nameError && showErrorMessage ? "inputError" : "input" }
           />
+          {emailError && showErrorMessage && (
+            <p style={{ color: "red" }}>This field is required</p>
+          )}
           <input
             placeholder="Email"
             type="text"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleEmailField}
             name="email"
+            className={emailError && showErrorMessage ? "inputError" : "input" }
           />
+          {messageError && showErrorMessage && (
+            <p style={{ color: "red" }}>This field is required</p>
+          )}
           <textarea
             placeholder="Message"
             type="text"
             rows="10"
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={handleMessageField}
             name="message"
+            className={messageError && showErrorMessage ? "textInputError" : "textInput" }
           />
-          <button className="submitForm">
-            Send
-          </button>
+          <button className="submitForm">Send</button>
         </form>
       </div>
     </div>
